1d25f1d939174f10967d34b085ddff9189480f1a
Nikolay Dymura <nikolay.dymura@gmail.com>
2022-11-22 18:44:05 +0200

    Added desktops for demo

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_core_image_filters
+import path_provider_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  CoreImageFiltersPlugin.register(with: registry.registrar(forPlugin: "CoreImageFiltersPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+}
