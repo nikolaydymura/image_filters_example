9a42efbb84fc15137926236e0185c8aa6d2a8f69
egorterekhov <asya.wear@gmail.com>
2023-02-06 23:19:29 -0800

    Deleted ListView builder as a duplicate

--- lib/widgets/list_supported_filters_widget.dart
+++ lib/widgets/list_supported_filters_widget.dart
@@ -65,59 +65,130 @@ class ListSupportedFiltersWidget extends StatelessWidget {
           ),
         ),
         Expanded(
-          child: CustomScrollView(
-            slivers: [
-              SliverFixedExtentList(
-                delegate: SliverChildBuilderDelegate(
-                  (context, index) {
-                    final item = filters[index];
-                    return Card(
-                      color: Colors.greenAccent[200],
-                      child: ListTile(
-                        trailing: const Icon(
-                          Icons.navigate_next,
-                        ),
-                        title: Text(
-                          item,
-                        ),
-                        onTap: () {
-                          onItemTap(item);
+          child: BlocBuilder<SearchBloc, SearchState>(
+            builder: (context, state) {
+              if (state is SearchEmpty) {
+                return const Center(
+                  child: Text(
+                    'Founded nothing',
+                    style: TextStyle(fontSize: 24),
+                  ),
+                );
+              } else if (state is SearchSucceeded) {
+                return CustomScrollView(
+                  slivers: [
+                    SliverFixedExtentList(
+                      delegate: SliverChildBuilderDelegate(
+                        (context, index) {
+                          final item = state.items[index];
+                          return Card(
+                            color: Colors.greenAccent[200],
+                            child: ListTile(
+                              trailing: const Icon(
+                                Icons.navigate_next,
+                              ),
+                              title: Text(
+                                item,
+                              ),
+                              onTap: () {
+                                onItemTap(item);
+                              },
+                            ),
+                          );
                         },
+                        childCount: state.items.length, // 1000 list items
                       ),
-                    );
-                  },
-                  childCount: filters.length, // 1000 list items
-                ),
-                itemExtent: 64,
-              ),
-              SliverFixedExtentList(
-                delegate: SliverChildBuilderDelegate(
-                  (context, index) {
-                    final item = items[index];
-                    return Card(
-                      child: ListTile(
-                        title: Text(item),
-                        trailing: Icon(
-                          Icons.navigate_next,
-                          color: Theme.of(context).primaryColor,
-                        ),
-                        onTap: () {
-                          onItemTap(item);
+                      itemExtent: 64,
+                    ),
+                    SliverFixedExtentList(
+                      delegate: SliverChildBuilderDelegate(
+                        (context, index) {
+                          final item = state.items[index];
+                          return Card(
+                            child: ListTile(
+                              title: Text(item),
+                              trailing: Icon(
+                                Icons.navigate_next,
+                                color: Theme.of(context).primaryColor,
+                              ),
+                              onTap: () {
+                                onItemTap(item);
+                              },
+                            ),
+                          );
                         },
+                        childCount: state.items.length,
                       ),
-                    );
-                  },
-                  childCount: items.length,
-                ),
-                itemExtent: 64,
-              )
-            ],
+                      itemExtent: 64,
+                    )
+                  ],
+                );
+              } else if (state is SearchInitial) {
+                return CustomScrollView(
+                  slivers: [
+                    SliverFixedExtentList(
+                      delegate: SliverChildBuilderDelegate(
+                        (context, index) {
+                          if (state is SearchEmpty) {
+                            return const Center(
+                              child: Text(
+                                'Founded nothing',
+                                style: TextStyle(fontSize: 24),
+                              ),
+                            );
+                          }
+                          final item = filters[index];
+                          return Card(
+                            color: Colors.greenAccent[200],
+                            child: ListTile(
+                              trailing: const Icon(
+                                Icons.navigate_next,
+                              ),
+                              title: Text(
+                                item,
+                              ),
+                              onTap: () {
+                                onItemTap(item);
+                              },
+                            ),
+                          );
+                        },
+                        childCount: filters.length, // 1000 list items
+                      ),
+                      itemExtent: 64,
+                    ),
+                    SliverFixedExtentList(
+                      delegate: SliverChildBuilderDelegate(
+                        (context, index) {
+                          final item = items[index];
+                          return Card(
+                            child: ListTile(
+                              title: Text(item),
+                              trailing: Icon(
+                                Icons.navigate_next,
+                                color: Theme.of(context).primaryColor,
+                              ),
+                              onTap: () {
+                                onItemTap(item);
+                              },
+                            ),
+                          );
+                        },
+                        childCount: items.length,
+                      ),
+                      itemExtent: 64,
+                    )
+                  ],
+                );
+              }
+              return const CircularProgressIndicator();
+            },
           ),
         ),
         const SizedBox(
           height: 1,
         ),
-        Expanded(
+        /*Expanded(
           child: BlocBuilder<SearchBloc, SearchState>(
             builder: (context, state) {
               if (state is SearchEmpty) {
@@ -171,7 +242,7 @@ class ListSupportedFiltersWidget extends StatelessWidget {
               return const CircularProgressIndicator();
             },
           ),
-        ),
+        ),*/
       ],
     );
   }
