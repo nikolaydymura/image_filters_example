1267e62ae35fc36d61d68d5469fe1e47f7c62bb8
egorterekhov <asya.wear@gmail.com>
2023-02-10 18:28:43 -0800

    Changed load file function. Use image picker

--- lib/blocs/source_image_bloc/source_image_bloc.dart
+++ lib/blocs/source_image_bloc/source_image_bloc.dart
@@ -1,12 +1,9 @@
 import 'dart:async';
-import 'dart:io';
+import 'dart:typed_data';
 
-import 'package:equatable/equatable.dart';
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_gpu_filters_interface/flutter_gpu_filters_interface.dart';
 import 'package:flutter_image_filters/flutter_image_filters.dart';
-import 'package:flutter_image_filters/flutter_image_filters.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:rxdart/rxdart.dart';
 
 part 'source_image_state.dart';
@@ -22,12 +19,15 @@ class SourceImageCubit extends Cubit<SourceImageState> {
       });
 
   Future<void> loadFile() async {
-    FilePickerResult? result = await FilePicker.platform.pickFiles();
+    ImagePicker? picker = ImagePicker();
 
-    if (result != null) {
-      final file = File(result.files.single.path ?? '');
-      final texture = await TextureSource.fromFile(file);
-      emit(SourceImageReady(texture, file.absolute.path, false));
+    final XFile? image = await picker.pickImage(source: ImageSource.gallery);
+
+    if (image != null) {
+      final Uint8List bytes = await image.readAsBytes();
+
+      final texture = await TextureSource.fromMemory(bytes);
+      emit(SourceImageReady(texture, image.path, false));
     } else {
       print('Nothing was selected');
     }
